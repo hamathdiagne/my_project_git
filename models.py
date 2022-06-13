299d93b15d8e00f7ef1c82b05652b5072460f4aa
hamathdiagne <88361325+hamathdiagne@users.noreply.github.com>
2022-06-13 03:46:50 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,13 @@
-class Article(modèles.Model):
+class Article(models.Model):
   titre=models.CharField(max_length=100)
   auteur=models.CharField(max_length=42)
   contenu=models.TextField(null=True)
+  categorie=models.ForeignKey('Categorie', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.titre
+ 
+class Categorie (models.Model):
+  nom=models.CharField(max_length=30)
+  def __str__(self):
+    return self.nom
